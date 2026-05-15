15db705ab68ce57cba005040ba141739fc41fab5
hasithadhananjaya69 <it25101867@my.sliit.lk>
2026-05-15 13:23:58 +0530

    Add DriverRepository

---
+++ Bike Rental and Ride-Sharing Platform/src/main/java/com/oop/oop_web_proj/repo/DriverRepository.java
@@ -0,0 +1,7 @@
+package com.oop.oop_web_proj.repo;
+
+import com.oop.oop_web_proj.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
