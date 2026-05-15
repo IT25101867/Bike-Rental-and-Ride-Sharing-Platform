376d95d5b12f95c1aa921fe3898a02438af6b773
hasithadhananjaya69 <it25101867@my.sliit.lk>
2026-05-15 18:30:29 +0530

    Update Driver.java

--- Bike Rental and Ride-Sharing Platform/src/main/java/com/oop/oop_web_proj/entity/Driver.java
+++ Bike Rental and Ride-Sharing Platform/src/main/java/com/oop/oop_web_proj/entity/Driver.java
@@ -16,15 +16,31 @@ public class Driver {
 
     public Driver() {}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getLicenseNumber() { return licenseNumber; }
-    public void setLicenseNumber(String licenseNumber) { this.licenseNumber = licenseNumber; }
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
 
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
