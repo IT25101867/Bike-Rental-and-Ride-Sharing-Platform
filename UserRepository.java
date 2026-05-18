83e883df21940a4d9a617a725761acb75497bad3
IT25101859 <IT25101859@my.sliit.lk>
2026-05-18 21:02:39 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    List<User> findByNameContainingIgnoreCase(String name);
+public interface UserRepository extends JpaRepository<User, Long> {//Interface Declaration
+    Optional<User> findByEmail(String email);//Custom method to find a user by using email
+    List<User> findByNameContainingIgnoreCase(String name);//Custom method to find a users by name
 }
